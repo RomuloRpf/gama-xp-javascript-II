4cd9197600e5ecd33a6173f1868f0caae65f5e49
Romulo Peigas <rpf.2008@yahoo.com.br>
2021-07-06 20:43:18 -0300

    JavaScript II || 03 - Operador Ternário

---
+++ aula3/operador-ternario.js
@@ -0,0 +1,12 @@
+function calculaIdade(idade){
+    /*if (idade >= 18){
+        return 'Maior de idade'
+    }
+    else{
+        return 'Menor de idade'
+    }*/
+
+    return idade >= 18? 'Maior de idade': 'Menor de idade';
+}
+
+console.log(calculaIdade(19))
