da25dba20632b341efae8c49f2b5532c16fc244a
Romulo Peigas <rpf.2008@yahoo.com.br>
2021-07-06 20:50:13 -0300

    JavaScript II || 04 - Arrow Functions

---
+++ aula4/arrow-funtion.js
@@ -0,0 +1,16 @@
+//function
+function soma(a,b){
+    return a + b
+}
+
+console.log(soma(3,5))
+
+//arrow function
+
+const soma2 = (num1,num2) => num1 + num2
+
+console.log(soma2(5,8))
+
+const sayHello = name => `Hello ${name}`
+
+console.log(sayHello("Pedro"))
