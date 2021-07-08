6e26c2750e2b72ec42e60a700e19eda979e458e9
Romulo Peigas <rpf.2008@yahoo.com.br>
2021-07-07 21:38:47 -0300

    JavaScript II || 05 - Arrays, Spread & Métodos

---
+++ aula5/arrays.js
@@ -0,0 +1,42 @@
+const alunasGama = ["Paula","Maria","Estela","Clara"]
+
+console.log(alunasGama[3])
+
+//Operador spread (...)
+
+const alunasXp = [...alunasGama, "Simara"]
+
+console.log(alunasXp)
+
+//Métodos de Iteração
+/*
+for (let i = 0; i < alunasXp.length; i++) {
+    console.log(alunasXp[i])
+}
+*/
+alunasXp.map(aluna => console.log(aluna))
+
+//Filter 
+const numeros = [34,45,67,80,90,55,76]
+
+const numerosImpares = numeros.filter(numero => numero%2 != 0)
+console.log(numerosImpares)
+
+const numerosPares = numeros.filter(numero => numero%2 == 0)
+console.log(numerosPares)
+
+//sort
+const numerosOrdemCrescente = numeros.sort()
+console.log(numerosOrdemCrescente)
+
+const numerosOrdemDecrescente = numeros.sort((a,b) => b-a)
+console.log(numerosOrdemDecrescente)
+
+//reduce - reduz nosso array a um resultado de uma operação matemática
+const numbers = [1,34,35]
+
+const soma = numbers.reduce((valorAnterior, valorAtual) => {
+    return valorAnterior + valorAtual
+},0)
+
+console.log(soma)
